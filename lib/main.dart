83f4f862c4b4c056036cc486a889621c8e5d3584
fillerInk <thatonephysicsnerd@gmail.com>
2019-11-10 18:34:15 +0530

    created homepage as stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
-  @override
-  MyAppState createState() => new MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,42 +13,53 @@ class MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Avengers Money Tracker'),
-            backgroundColor: Colors.blueGrey,
-          ),
-          body: ListView(
-            children: <Widget>[
-              PersonCard(),
-              PersonCard(),
-              PersonCard(),
-              SizedBox(
-                height: 35,
-              ),
-              PersonCard(),
-              PersonCard(),
-              PersonCard(),
-              SizedBox(
-                height: 35,
-              ),
-              PersonCard(),
-              PersonCard(),
-              PersonCard(),
-              SizedBox(
-                height: 35,
-              ),
-              PersonCard(),
-              PersonCard(),
-              PersonCard(),
-            ],
-          ),
-        ));
+            appBar: AppBar(
+              title: Text('Avengers Money Tracker'),
+              backgroundColor: Colors.blueGrey,
+            ),
+            body: new HomePage()));
   }
 }
 
-Widget _buildList(BuildContext context){
-  return StreamBuilder
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
 }
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        PersonCard(),
+        PersonCard(),
+        PersonCard(),
+        SizedBox(
+          height: 35,
+        ),
+        PersonCard(),
+        PersonCard(),
+        PersonCard(),
+        SizedBox(
+          height: 35,
+        ),
+        PersonCard(),
+        PersonCard(),
+        PersonCard(),
+        SizedBox(
+          height: 35,
+        ),
+        PersonCard(),
+        PersonCard(),
+        PersonCard(),
+      ],
+    );
+  }
+}
+
+//Widget _buildList(BuildContext context){
+//  return StreamBuilder
+//}
 
 class PersonCard extends StatefulWidget {
 //  final String text;
@@ -65,42 +69,39 @@ class PersonCard extends StatefulWidget {
   PersonCardState createState() => new PersonCardState();
 }
 
-class PersonCardState extends State<PersonCard> {
 
-  int cash =0;
+
+class PersonCardState extends State<PersonCard> {
+  int cash = 0;
 
   @override
   Widget build(BuildContext context) {
-        return Card(
-          child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 30, left: 20, bottom: 30),
-                  child: Text(
-                    snapshot.data.documents['name'],
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(right: 30),
-                    child: SizedBox(
-                      width: 100,
-                      child: TextFormField(
-                        initialValue: snapshot.data.documents['cash'],
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-
-                      ),
-                    ))
-              ],
+    return Card(
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 20, bottom: 30),
+              child: Text(
+                "hello",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),);
-      },
-//
+            Padding(
+                padding: EdgeInsets.only(right: 30),
+                child: SizedBox(
+                  width: 100,
+                  child: TextFormField(
+                    initialValue: '0',
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
-    }
   }
-
+}
